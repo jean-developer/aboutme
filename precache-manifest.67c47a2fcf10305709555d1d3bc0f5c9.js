3e37e68bef0e72150c7fe8768aa38b916b73d7ce
jean-developer <jean-developer@users.noreply.github.com>
2021-12-27 12:17:44 +0000

    Deploying to gh-pages from  @ 5543d0660acf7700287a1dacdccf4a15bc2cab89 🚀

---
+++ precache-manifest.67c47a2fcf10305709555d1d3bc0f5c9.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c8c6454149d9c8641f58bd513cdc67e",
+    "url": "/aboutme/index.html"
+  },
+  {
+    "revision": "a72b26bfe3aa50c22adc",
+    "url": "/aboutme/static/css/3.6a352120.chunk.css"
+  },
+  {
+    "revision": "ac0889c898065116add6",
+    "url": "/aboutme/static/css/4.6e14a8dd.chunk.css"
+  },
+  {
+    "revision": "fd9d3db9af8efbcc8543",
+    "url": "/aboutme/static/css/main.061ca5c0.chunk.css"
+  },
+  {
+    "revision": "448b2767961cc4ab6b49",
+    "url": "/aboutme/static/js/2.3dd686e7.chunk.js"
+  },
+  {
+    "revision": "3dbd45c8ed21d092cdcb3d5851d22288",
+    "url": "/aboutme/static/js/2.3dd686e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a72b26bfe3aa50c22adc",
+    "url": "/aboutme/static/js/3.40c148de.chunk.js"
+  },
+  {
+    "revision": "ac0889c898065116add6",
+    "url": "/aboutme/static/js/4.a7b6de9d.chunk.js"
+  },
+  {
+    "revision": "fd9d3db9af8efbcc8543",
+    "url": "/aboutme/static/js/main.501446c5.chunk.js"
+  },
+  {
+    "revision": "a9f75f4cc8ec705371cc",
+    "url": "/aboutme/static/js/runtime-main.c2213565.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/aboutme/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/aboutme/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "db514d0c6701b054a0abdd35634033ca",
+    "url": "/aboutme/static/media/apaplogo.db514d0c.jpeg"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/aboutme/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/aboutme/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/aboutme/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "05b1b0ecc995fb4a53627de914917ec7",
+    "url": "/aboutme/static/media/endurancelogo.05b1b0ec.png"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/aboutme/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "86159da683e62ef8005f959c54007e2c",
+    "url": "/aboutme/static/media/itfamlogo.86159da6.png"
+  },
+  {
+    "revision": "2d827b7e6df8b4f5d1f3190e54be413a",
+    "url": "/aboutme/static/media/itlalogo.2d827b7e.png"
+  },
+  {
+    "revision": "6a48c99478e115a49c3240353ce00a85",
+    "url": "/aboutme/static/media/logicOneLogo.6a48c994.png"
+  },
+  {
+    "revision": "7e8995324337a910af367570d3ce9370",
+    "url": "/aboutme/static/media/logo-unapec.7e899532.jpeg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/aboutme/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "fe772a0078ab80b7533bc8086de2154c",
+    "url": "/aboutme/static/media/pslearninglogo.fe772a00.webp"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/aboutme/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/aboutme/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/aboutme/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
