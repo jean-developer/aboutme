82d7b85f173d0af57d5b03c9cca95a19ce5df6e8
Jean Carlos Arnaud B <jeancarlos2704@hotmail.com>
2022-03-01 14:41:49 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/aboutme/precache-manifest.778578bd364b71d79e11c5f41b20c0a5.js"
+  "/aboutme/precache-manifest.03526d7de53db2d5f9340225df192187.js"
 );
 
 self.addEventListener('message', (event) => {
